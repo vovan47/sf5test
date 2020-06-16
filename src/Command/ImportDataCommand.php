adb0f8c616fd42e4deda7137e0cfa7291eec66ba
Vladimir <vladimir@vakhon.in>
2020-06-16 17:35:51 +0700

    working command, added fixtures for testing

--- src/Command/ImportDataCommand.php
+++ src/Command/ImportDataCommand.php
@@ -2,6 +2,10 @@
 
 namespace App\Command;
 
+use App\Entity\Category;
+use App\Service\CategoryService;
+use App\Service\ProductService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -11,6 +15,32 @@ class ImportDataCommand extends Command
 {
     const TYPE_PRODUCT = 'product';
     const TYPE_CATEGORY = 'category';
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /** @var EntityManagerInterface */
+    protected $em;
+
+    /**
+     * @var CategoryService
+     */
+    protected $categoryService;
+
+    /**
+     * @var ProductService
+     */
+    protected $productService;
+
+    public function __construct(?string $name = null, EntityManagerInterface $em, CategoryService $categoryService, ProductService $productService)
+    {
+        $this->em = $em;
+        $this->categoryService = $categoryService;
+        $this->productService = $productService;
+        parent::__construct($name);
+    }
 
     protected function configure()
     {
@@ -23,6 +53,12 @@ class ImportDataCommand extends Command
                 InputOption::VALUE_REQUIRED,
                 'Entity type: product or category'
             )
+            ->addOption(
+                'file',
+                'f',
+                InputOption::VALUE_REQUIRED,
+                'Path to file with JSON data'
+            )
         ;
     }
 
@@ -31,14 +67,105 @@ class ImportDataCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
+
         $type = $input->getOption('type');
 
         if (!in_array($type, [self::TYPE_PRODUCT, self::TYPE_CATEGORY])) {
             throw new \InvalidArgumentException('Wrong type option specified');
         }
 
+        if ($filename = $input->getOption('file')) {
+            $contents = file_get_contents($filename);
+        } else if (0 === ftell(STDIN)) {
+            $contents = '';
+            while (!feof(STDIN)) {
+                $contents .= fread(STDIN, 1024);
+            }
+        } else {
+            throw new \RuntimeException("Please provide a filename or pipe template content to STDIN.");
+        }
+
         $output->writeln('Starting import');
+        if ($type === self::TYPE_CATEGORY) {
+            $count = $this->importCategories($contents);
+        } else if ($type === self::TYPE_PRODUCT) {
+            $count =  $this->importProducts($contents);
+        }
+        $output->writeln(sprintf('Imported %d entities of type "%s"', $count, $type));
         $output->writeln('Finished import');
         return 0;
+    }
+
+    /**
+     * @param string $content
+     * @return int
+     * @throws \Exception
+     */
+    protected function importCategories($content)
+    {
+        $handler = $this->categoryService;
+        $array = json_decode($content, true);
+        if (!is_array($array)) {
+            throw new \Exception('Invalid JSON');
+        }
+        $count = 0;
+        foreach ($array as $key => $row) {
+            try {
+                $form = $handler->createForm(null, [
+                    'http_method' => 'POST',
+                ]);
+                $row['eid'] = $row['eId'];
+                $form->submit($row);
+                if (!$handler->isPostValid($form)) {
+                    throw new \Exception(sprintf('Row %s is not valid', var_export($row)));
+                }
+
+                $category = $handler->persist($form);
+                $handler->flush();
+
+                $count++;
+            } catch (\Exception $e) {
+                $this->output->writeln(sprintf('Error on row %d: %s', $key+1, $e->getMessage()));
+            }
+        }
+        return $count;
+    }
+
+    /**
+     * @param string $content
+     * @return int
+     * @throws \Exception
+     */
+    protected function importProducts($content)
+    {
+        $handler = $this->productService;
+        $array = json_decode($content, true);
+        if (!is_array($array)) {
+            throw new \Exception('Invalid JSON');
+        }
+        $count = 0;
+        foreach ($array as $key => $row) {
+            try {
+                $form = $handler->createForm(null, [
+                    'http_method' => 'POST',
+                ]);
+                $row['eid'] = $row['eId'];
+                $row['categories'] = $row['categoriesEId'] ?? $row['categoryEId'] ?? null;
+                $form->submit($row);
+                if (!$handler->isPostValid($form)) {
+                    throw new \Exception(sprintf('Row %s is not valid', var_export($row)));
+                }
+
+                $product = $handler->persist($form);
+                $handler->flush();
+
+                $count++;
+            } catch (\Exception $e) {
+                $this->output->writeln(sprintf('Error on row %d: %s', $key+1, $e->getMessage()));
+                $this->output->writeln('Row content: ' . var_export($row));
+            }
+        }
+        return $count;
     }
 }
--- src/Service/CategoryService.php
+++ src/Service/CategoryService.php
@@ -2,9 +2,12 @@
 
 namespace App\Service;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormInterface;
 
-class CategoryService
+class CategoryService extends AbstractService
 {
     /**
      * @var CategoryRepository
@@ -19,8 +22,24 @@ class CategoryService
         return $this->repository;
     }
 
-    public function __construct(CategoryRepository $repository)
+    public function __construct(string $formClassName, CategoryRepository $repository, EntityManagerInterface $em)
     {
         $this->repository = $repository;
+        $this->em = $em;
+        parent::__construct($formClassName);
+    }
+
+    /**
+     * @param FormInterface $form
+     * @return Category
+     */
+    public function persist(FormInterface $form)
+    {
+        /** @var Category $category */
+        $category = $form->getData();
+
+        $this->em->persist($category);
+
+        return $category;
     }
 }
